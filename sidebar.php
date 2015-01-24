5f280b82d19d7baf9f62cd657be350f99a91a0e1
mick90207 <b98502030@ntu.edu.tw>
2015-01-24 18:36:12 +0800

    Beta_1

---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">SB Admin</a>
+            </div>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> 全臺捐血情形</a>
+                    </li>
+                    <li>
+                        <a href="map.php"><i class="fa fa-fw fa-bar-chart-o"></i>捐血地圖</a>
+                    </li>
+                    <li>
+                        <a href="QA.html"><i class="fa fa-fw fa-table"></i>捐血小知識</a>
+                    </li>
+                    <li>
+                        <a href="mobile.php"><i class="fa fa-fw fa-table"></i>APPdemo</a>
+                    </li>
+                        
+                    
+            <!-- /.navbar-collapse -->
+        </nav>
